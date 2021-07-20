8afff980b7694b6632526eebeefb8015bcfe7c24
Valmond Williams <v-valmond.williams@formidable.com>
2021-07-20 02:30:35 +0100

    add renove change1

--- test/pageobjects/addRemovePage.js
+++ test/pageobjects/addRemovePage.js
@@ -1,43 +1,43 @@
 class AddRemovePage {
-	get add() {
-		return $("#content > div > button");
-	}
+  get add() {
+    return $("#content > div > button");
+  }
 
-	get pageHeader() {
-		return $("#content > h3");
-	}
+  get pageHeader() {
+    return $("#content > h3");
+  }
 
-	get deleteButton() {
-		return $("#elements .added-manually");
-	}
+  get deleteButton() {
+    return $("#elements .added-manually");
+  }
 
-	gotoAddRemovePage() {
-		browser.url("/add_remove_elements/");
-		browser.maximizeWindow();
-		expect(this.pageHeader).toHaveTextContaining("Add/Remove Elements");
-	}
+  gotoAddRemovePage() {
+    browser.url("/add_remove_elements/");
+    browser.maximizeWindow();
+    expect(this.pageHeader).toHaveTextContaining("Add/Remove Elements");
+  }
 
-	addElement(num) {
-		if (num < 1) {
-			throw "number must be 1 or more";
-		}
-		for (let i = 1; i <= num; i++) {
-			this.add.click();
-		}
-	}
+  addElement(num) {
+    if (num < 1) {
+      throw "number must be 1 or more";
+    }
+    for (let i = 1; i <= num; i++) {
+      this.add.click();
+    }
+  }
 
-	deleteElement(num) {
-		if (num < 1) {
-			throw "number must be 1 or more";
-		}
-		for (let i = 1; i <= num; i++) {
-			this.deleteButton.click();
-		}
-	}
+  deleteElement(no) {
+    if (no < 1) {
+      throw "number must be 1 or more";
+    }
+    for (let i = 1; i <= no; i++) {
+      this.deleteButton.click();
+    }
+  }
 
-	get getElementCount() {
-		return $$("#elements .added-manually").length;
-	}
+  get getElementCount() {
+    return $$("#elements .added-manually").length;
+  }
 } //class
 
 module.exports = new AddRemovePage();
